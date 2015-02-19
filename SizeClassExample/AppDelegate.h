e8fff2a0f107d3b9dc11aa0913005577fdf48e50
Ben Norris <ben@bsn.io>
2015-02-19 15:03:13 -0700

    Initial Commit

---
+++ SizeClassExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SizeClassExample
+//
+//  Created by Ben Norris on 2/19/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SizeClassExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SizeClassExample
+//
+//  Created by Ben Norris on 2/19/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
